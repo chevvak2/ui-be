be3619bc2dffa06e61e43e1197083c1c7b5e81ad
chevvak2 <114002880+chevvak2@users.noreply.github.com>
2026-03-19 12:28:11 -0400

    Update build-docker-container.sh

--- build-docker-container.sh
+++ build-docker-container.sh
@@ -1,65 +1,70 @@
 #!/bin/sh
-
 set -e
 
 usage()
 {
-echo "$0 -b <be-branch-name> -f <fe-branch-name> [-i <image_name>] [-t <target_stage>]"
-exit 1
+    echo "$0 -b <be-branch-name> -f <fe-branch-name> [-i <image_name>] [-t <target_stage>]"
+    exit 1
 }
 
 if [ $# -lt 4 ]; then
-usage
-exit 1
+    usage
 fi
 
+# defaults
 image_name="translator-app"
 target_stage="cron"  # default stage
 
+# parse args
 while getopts 'b:f:i:t:' opt
 do
-case $opt in
-b) be_branch="$OPTARG" ;;
-f) fe_branch="$OPTARG" ;;
-i) image_name="$OPTARG" ;;
-t) target_stage="$OPTARG" ;;
-?) usage ;;
-esac
+    case $opt in
+        b) be_branch="$OPTARG" ;;
+        f) fe_branch="$OPTARG" ;;
+        i) image_name="$OPTARG" ;;
+        t) target_stage="$OPTARG" ;;
+        ?) usage ;;
+    esac
 done
+
+# allow env override
+target_stage=${TARGET_STAGE:-$target_stage}
 
 echo "fe-branch: $fe_branch; be_branch: $be_branch; image_name: $image_name; target: $target_stage"
 
-# Save current branch
-
+# save current branch
 save_branch=$(git branch --show-current)
 
+# checkout backend
 echo "Checking out be-branch $be_branch"
 git checkout "$be_branch"
 git pull
-
 be_tag=$(git rev-parse --short HEAD)
 
-# Clone FE (no build here)
-
+# clone frontend
 ./build-fe.sh "$fe_branch" no
-
 cd ui-fe
 fe_tag=$(git rev-parse --short HEAD)
 
-timestamp=$(date -u "+%Y.%m.%d_%H.%M.%SZ")  # underscores instead of *
+# version tag
+timestamp=$(date -u "+%Y.%m.%d_%H.%M.%SZ")
 version_tag="FE.${fe_tag}_BE.${be_tag}_$timestamp"
+version_tag=$(echo "$version_tag" | tr '*' '_')
 
-# Inject build metadata
-
+# inject FE metadata
 echo "VITE_BUILD_INFO=$version_tag" > .env
-
 cd ..
 
+# build Docker
 echo "Building Docker image with target: $target_stage"
-
 docker build --no-cache --target "$target_stage" -t "$image_name:$version_tag" -t "$image_name" .
 
 echo "Docker image built: $image_name:$version_tag"
 
-echo "Restoring branch $save_branch"
-git checkout "$save_branch"
+# restore branch safely
+if [ -n "$save_branch" ]; then
+    echo "Restoring branch $save_branch"
+    git checkout "$save_branch"
+else
+    echo "No branch to restore (detached HEAD)"
+fi
