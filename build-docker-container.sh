9d862c0fbb685c60133247581ee7f1b8b0cb2054
chevvak2 <114002880+chevvak2@users.noreply.github.com>
2026-03-19 12:18:27 -0400

    Update build-docker-container.sh

--- build-docker-container.sh
+++ build-docker-container.sh
@@ -4,49 +4,64 @@ set -e
 
 usage()
 {
-    echo "$0 -b <be-branch-name> -f <fe-branch-name> [-i <image_name>]"
-    exit 1
+echo "$0 -b <be-branch-name> -f <fe-branch-name> [-i <image_name>] [-t <target_stage>]"
+exit 1
 }
 
 if [ $# -lt 4 ]; then
-    usage
-    exit 1
+usage
+exit 1
 fi
 
 image_name="translator-app"
-while getopts 'b:f:i:' opt
+target_stage="cron"  # default stage
+
+while getopts 'b:f:i:t:' opt
 do
-    case $opt in
-        b) be_branch="$OPTARG" ;;
-        f) fe_branch="$OPTARG" ;;
-        i) image_name="$OPTARG" ;;
-        ?) usage ;;
-    esac
+case $opt in
+b) be_branch="$OPTARG" ;;
+f) fe_branch="$OPTARG" ;;
+i) image_name="$OPTARG" ;;
+t) target_stage="$OPTARG" ;;
+?) usage ;;
+esac
 done
 
-echo "fe-branch: $fe_branch; be_branch: $be_branch; image_name: $image_name"
+echo "fe-branch: $fe_branch; be_branch: $be_branch; image_name: $image_name; target: $target_stage"
 
-# This script ensures the BE repo is on the right branch,
-# that the FE repo is cloned and on the right branch (via a script),
-# and then builds and tags the image
+# Save current branch
 
 save_branch=$(git branch --show-current)
+
 echo "Checking out be-branch $be_branch"
-git checkout $be_branch
+git checkout "$be_branch"
 git pull
+
 be_tag=$(git rev-parse --short HEAD)
-# Clone but do not build the FE dependencies; do that inside the container
+
+# Clone FE (no build here)
+
 ./build-fe.sh "$fe_branch" no
+
 cd ui-fe
 fe_tag=$(git rev-parse --short HEAD)
 timestamp=$(date -u "+%Y.%m.%dt%H.%M.%Sz")
-version_tag="FE.${fe_tag}_BE.${be_tag}_$timestamp"
-# Confirmed with FE team that clobbering the .env file is acceptable 2024-12-28 NP
+version_tag="FE.${fe_tag}*BE.${be_tag}*$timestamp"
+
+# Inject build metadata
+
 echo "VITE_BUILD_INFO=$version_tag" > .env
+
 cd ..
-# Removing latest tag
-docker build --no-cache -t "$image_name:$version_tag" -t "$image_name" --target app .
-docker build --no-cache -t pubsub --target cron .
-# Build pubsub cron job container
-echo "restoring branch $save_branch"
-git checkout $save_branch
+
+echo "Building Docker image with target: $target_stage"
+
+docker build --no-cache 
+--target "$target_stage" 
+-t "$image_name:$version_tag" 
+-t "$image_name" .
+
+echo "Docker image built: $image_name:$version_tag"
+
+echo "Restoring branch $save_branch"
+git checkout "$save_branch"
